94c1a01fd41da47d6997c79348e5fcaf42ee0760
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-04-04 00:15:40 +0530

    .gitignore is now working

---
+++ src/Backend/Entity/Migrations/20230403183504_create_initial_tables.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity.Migrations
+{
+    public partial class create_initial_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SerialNumber = table.Column<string>(maxLength: 100, nullable: true),
+                    Model = table.Column<int>(nullable: false),
+                    BatteryCapacity = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    State = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drone", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medication",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Weight = table.Column<int>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drone");
+
+            migrationBuilder.DropTable(
+                name: "Medication");
+        }
+    }
+}
